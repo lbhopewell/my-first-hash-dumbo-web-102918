713831bb4026b0c2e1c3ffa3b778c07fd000a1f5
Bruce Hopewell <lbhopewell@outlook.com>
2018-10-24 11:32:25 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,8 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-my_hash = {"my_name" => "Bruce", "my_city" 
-  
+your_hash = {"my_name" => "Bruce", "my_city" => "NYC"}
+your_hash
+
 end
 
 
